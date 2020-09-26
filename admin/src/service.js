804c9167e8446d389bd809aa32f911a7ad7d040c
bud aminof <budaminof@gmail.com>
2020-09-26 11:35:18 -0600

    call server

--- admin/src/service.js
+++ admin/src/service.js
@@ -2,22 +2,22 @@ import axios from 'axios';
 
 export default {
   getAllProjects() {
-    return axios.get('http://localhost:3000/')
+    return axios.get('https://immense-bastion-45421.herokuapp.com/')
       .then(res => res.data).catch(err => console.log('cant get all projects', err))
   },
 
   getProject(id) {
-    return axios.get('http://localhost:3000/project', { params: { id }})
+    return axios.get('https://immense-bastion-45421.herokuapp.com/project', { params: { id }})
       .then(res => res.data[0]).catch(err => console.log('cant get all projects', err))
   },
 
   addNewProject(name) {
-    return axios.post('http://localhost:3000/', { name })
+    return axios.post('https://immense-bastion-45421.herokuapp.com/', { name })
       .then(res => res.data._id).catch(err => console.log('could not add new project', err))
   },
 
   saveScreens(id, screens) {
-    return axios.post('http://localhost:3000/screens', { id, screens })
+    return axios.post('https://immense-bastion-45421.herokuapp.com/screens', { id, screens })
       .then(res => res).catch(err => console.log('could not add new project', err))
   },
 };
